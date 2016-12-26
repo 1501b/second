be526dde47f2de972e471037728164ef1d075694
caojun123 <caojun@blinnnk.com>
2016-12-26 16:51:13 +0800

    add GitTestActivity

---
+++ app/src/main/java/com/example/nightmodel/app/test/GitTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.nightmodel.app.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cj on 2016/12/26.
+ */
+
+public class GitTestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
